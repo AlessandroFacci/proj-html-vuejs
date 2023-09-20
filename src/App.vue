55a840b74f917a5ba380c9c7dacf8c25e3737c23
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-09-20 09:31:21 +0200

    App: import components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+
 export default {
   data() {
     return {};
+  },
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
   },
 };
 </script>
