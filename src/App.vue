09088b3b797fea037407ad3851a5350984a33dea
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-09-20 11:31:00 +0200

    App: delet components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
-import AppMain from "./components/AppMain.vue";
-import AppFooter from "./components/AppFooter.vue";
 
 export default {
   data() {
     return {};
   },
-  components: {
-    AppHeader,
-    AppMain,
-    AppFooter,
-  },
+  components: { AppHeader },
 };
 </script>
 
