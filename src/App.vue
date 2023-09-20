a9c33b8d89203ba63e75b5c42bf9db10a9944e98
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-09-20 11:39:52 +0200

    App: fix

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
 
 export default {
   data() {
     return {};
   },
-  components: { AppHeader },
+  components: { AppHeader, AppMain },
 };
 </script>
 
 <template>
   <AppHeader></AppHeader>
+  <AppMain></AppMain>
 </template>
 
 <style lang="scss">
