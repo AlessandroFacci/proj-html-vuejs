71777905a54bdbd88ea14bc5c1a7f6db7faffe1b
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-09-20 17:33:15 +0200

    AppCard: create and edit

---
+++ src/components/AppCard.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <div class="container-fluid d-flex justify-content-between">
+    <div class="card" style="width: 18rem">
+      <img src="../../public/img/service6-2x.jpg" />
+    </div>
+    <div class="card" style="width: 18rem">
+      <img src="../../public/img/service6-2x.jpg" />
+    </div>
+    <div class="card" style="width: 18rem">
+      <img src="../../public/img/service6-2x.jpg" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  border: 0;
+  border-radius: 0 0 2rem;
+  img {
+    border-radius: 0 0 2rem;
+  }
+}
+</style>
