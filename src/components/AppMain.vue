c48f0e4d155d74e12a2e3de7d1816518ad78c679
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-09-22 10:05:33 +0200

    AppMain: import Section2

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,7 @@
 <script>
 import AppHero from "./AppHero.vue";
 import Section1 from "./Section1.vue";
+import Section2 from "./Section2.vue";
 
 export default {
   data() {
@@ -9,6 +10,7 @@ export default {
   components: {
     AppHero,
     Section1,
+    Section2,
   },
 };
 </script>
@@ -16,6 +18,7 @@ export default {
 <template>
   <AppHero></AppHero>
   <Section1></Section1>
+  <Section2></Section2>
 </template>
 
 <style lang="scss" scoped></style>
