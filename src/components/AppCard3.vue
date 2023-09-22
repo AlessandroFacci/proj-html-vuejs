51e46b8d19350a9f6dba27b34ce6c6669cc578ed
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-09-22 11:36:25 +0200

    AppCard3: edit

--- src/components/AppCard3.vue
+++ src/components/AppCard3.vue
@@ -1,11 +1,34 @@
 <script>
 export default {
   props: {
-    cardInfo: Object,
+    cardArticle: Object,
   },
 };
 </script>
 
-<template></template>
+<template>
+  <div class="card" style="width: 19rem">
+    <img :src="cardArticle.image" />
+    <div class="card-content">
+      <h5>{{ cardArticle.title }}</h5>
+      <p>{{ cardArticle.description }}</p>
+    </div>
+  </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card {
+  border: none;
+  margin-top: 3rem;
+  .card-content {
+    padding-left: 1rem;
+    h5 {
+      margin-top: 1rem;
+    }
+    p {
+      color: var(--text-color-gray);
+      font-size: 0.7rem;
+    }
+  }
+}
+</style>
